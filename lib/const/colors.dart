9a3e18712c3d6b74986270d4a645bb3251684a42
학림 <63008138+hfjxjjd123@users.noreply.github.com>
2022-12-26 23:44:12 +0900

    remove git file

--- lib/const/colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color splashColor = Colors.greenAccent;
-Color onColor = Color.fromARGB(255, 4, 191, 157);
-Color fadeColor = Color.fromARGB(105, 4, 191, 157);
-Color offColor = Color.fromARGB(255, 23, 32, 38);
--- lib/const/fetching_analysis_flag.dart
+++
@@ -1,8 +0,0 @@
-const int ADD_NEW = 0;
-const int DELETE_DONE = 2;
-const int DELETE_DO = 1;
-const int DONE_TO_DO = 3;
-const int DO_TO_DONE = 4;
-const int RENAME = 5;
-const int READ = 6;
-const int MULTIPLE_ADD = 7;
