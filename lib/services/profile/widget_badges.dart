93fd7052c09e3f79b6f051a25ddcfdff56d1745f
hfjxjjd123 <hakrim@ihaglim-ui-MacBookAir.local>
2023-01-04 16:23:49 +0900

    z

--- lib/services/profile/widget_badges.dart
+++ lib/services/profile/widget_badges.dart
@@ -18,7 +18,7 @@ class WidgetBadges extends StatelessWidget {
       children: [
         Icon(Icons.local_fire_department, color: Colors.red,size: SIZE.width*0.08,),
         Container(width: SIZE.width * 0.03,),
-        Text('14+',style: Theme.of(context).textTheme.headline6!.copyWith(fontSize: SIZE.width*0.05),),
+        Text('구현중..',style: Theme.of(context).textTheme.headline6!.copyWith(fontSize: SIZE.width*0.05),),
         Container(width: SIZE.width * 0.03,),
         Icon(Icons.nightlight, color: Colors.amber,size: SIZE.width*0.08,),
       ],
